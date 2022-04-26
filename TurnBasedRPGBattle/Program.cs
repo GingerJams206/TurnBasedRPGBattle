d9b79b6c348ba56a2235caeed16b5282fe353e45
Sawyer Lightsey <sawyerlightsey@gmail.com>
2022-04-26 00:46:37 -0400

    commit test two

--- TurnBasedRPGBattle/Program.cs
+++ TurnBasedRPGBattle/Program.cs
@@ -10,7 +10,7 @@ namespace TurnBasedRPGBattle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test two");
         }
     }
 }
